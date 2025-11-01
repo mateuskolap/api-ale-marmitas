4e81f9a16847a3390ee606f2d57f92b951fd466b
mateuskolap <mateuskolap@gmail.com>
2025-11-01 15:26:33 -0300

    feat(fixtures): adiciona UserFixture para criar um usuário admin com email e senha a partir de variáveis de ambiente

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
---
+++ src/DataFixtures/UserFixture.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\DTO\Input\User\UserCreateInput;
+use App\Enum\Role;
+use App\Service\UserService;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixture extends Fixture
+{
+    public function __construct(
+        private readonly UserService $userService,
+    )
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $userAdmin = new UserCreateInput(
+            email: $_ENV['ADMIN_USER_EMAIL'],
+            roles: [Role::ADMIN->value],
+            password: $_ENV['ADMIN_USER_PASSWORD'],
+            passwordConfirmation: $_ENV['ADMIN_USER_PASSWORD'],
+        );
+
+        try {
+            $this->userService->create($userAdmin);
+        } catch (\InvalidArgumentException $e) {
+            // User already exists, do nothing
+        }
+    }
+}
