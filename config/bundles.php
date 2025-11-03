e7518ffe3ed457495ff93adeb269429c5da93158
mateuskolap <mateuskolap@gmail.com>
2025-11-03 03:49:32 -0300

    feat(refresh-token): implementa JWT refresh token e atualiza configurações de segurança

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
